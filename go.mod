087499b4f93e5ca8aa576194b793bd5cc654f85b
Aarya <falconblaze90@gmail.com>
2025-06-16 14:27:24 +0530

    initial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ErebusAJ/go-cli
+
+go 1.23.2
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func greetCmd(args []string) {
+	name := flag.String("name", "World", "Your name")
+	flag.CommandLine.Parse(args)
+	fmt.Printf("Hello, %s \n", *name)
+}
+
+func infoCmd(args []string) {
+	age := flag.Int("age", 0, "Your age")
+	flag.CommandLine.Parse(args)
+	
+	if *age > 0 {
+		fmt.Printf("You are %d years old \n", *age)
+	} else {
+		fmt.Printf("Age not provided! \n")
+	}
+}
+
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go-cli [command] [flags]")
+		return
+	}
+
+	switch os.Args[1] {
+	case "greet":
+		greetCmd(os.Args[2:])
+	
+	case "info":
+		infoCmd(os.Args[2:])
+	
+	default:
+		fmt.Println("Unknown command: ", os.Args[1])
+	}
+
+}
